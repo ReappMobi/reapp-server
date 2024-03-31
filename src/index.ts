81d2ec2350abaf0cf25f0d3e43940c9caf7bb33f
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-03-31 17:22:31 -0300

    feat: add express setup and hello world route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello, world!!")
+import * as express from "express"
+import { Request, Response } from "express"
+
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello, world!")
+})
+
+app.listen(3000, () => console.log("Listening on port 3000!"))
