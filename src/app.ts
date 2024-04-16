cb08a6269aedb68b74e055135611c33390ddecdf
Andre Gabriel <andre.gabriel@discente.ufma.br>
2024-04-16 11:44:35 -0300

    fix(app): fix import in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Express } from 'express';
 
-import { API_ROOT_PATH } from 'config/consts';
+import { API_ROOT_PATH } from '@config/consts';
 import router from '@api/routes';
 
 const app: Express = express();
