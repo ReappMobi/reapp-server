cc32e4ca3b862d1b8b0766200d42b0d0cc9d78ce
Andre Gabriel <andre.gabriel@discente.ufma.br>
2024-04-16 10:27:25 -0300

    feat: add api router to app
    - Add application api router to app.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { Express } from 'express';
 
+import { API_ROOT_PATH } from 'config/consts';
+import router from '@api/routes';
+
 const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(API_ROOT_PATH, router);
 
 export default app;
