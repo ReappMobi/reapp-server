9f4ac386042a07813b83730f7d86a7d5725d943f
Andre Gabriel <andre.gabriel@discente.ufma.br>
2024-04-27 15:52:08 -0300

    feat(routes): integrate Donnor router
    - Integrate the Donnor router into the main API router.

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import healthcheck from '@components/healthcheck/healthcheck.router';
+import donnor from '@components/donnor/donnor.router';
 
 const router: Router = Router();
 
 router.use(healthcheck);
+router.use(donnor);
 
 export default router;
