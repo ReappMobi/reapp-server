08d8a1734a4379873cf5a852ec04e81e8fdeefad
Andre Gabriel <andre.gabriel@discente.ufma.br>
2024-04-27 12:45:07 -0300

    chore(vitest): refactor vite test configuration
    - Refactor vite test configuration to utilize vite-tsconfig-paths plugin
    for improved TypeScript path resolution.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,9 @@
-import path from 'path';
 import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     exclude: ['node_modules', 'dist', 'coverage', 'pg-data'],
-  },
-  resolve: {
-    alias: {
-      '@app': path.resolve(__dirname, './src/app.ts'),
-      '@api': path.resolve(__dirname, './src/api/'),
-      '@config': path.resolve(__dirname, './src/config'),
-      '@components': path.resolve(__dirname, './src/api/components/'),
-      '@services': path.resolve(__dirname, './src/services/'),
-    },
   },
 });
