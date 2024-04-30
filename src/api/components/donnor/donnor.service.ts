9dab08865e515faef662754ca6997abaaf38e5bb
Andre Gabriel <andre.gabriel@discente.ufma.br>
2024-04-29 21:45:00 -0300

    refactor(api): improve DonnorService createDonnor method
    - Refactor the createDonnor method in DonnorService to improve error
    handling and readability.

--- src/api/components/donnor/donnor.service.ts
+++ src/api/components/donnor/donnor.service.ts
@@ -1,13 +1,29 @@
 import { Prisma } from '@prisma/client';
 
+import HttpStatus from '@services/http-status';
+import prisma from '@services/client';
+
+import { AppError } from 'utils/AppError';
+
 export class DonnorService {
-  private prisma: PrismaClient;
-
-  constructor() {
-    this.prisma = new PrismaClient();
-  }
-
   public async createDonnor(donnor: Prisma.DonnorCreateInput) {
-    return await this.prisma.donnor.create({ data: donnor });
+    const donnorExists = await prisma.donnor.findUnique({
+      where: {
+        email: donnor.email,
+      },
+    });
+
+    if (donnorExists) {
+      throw new AppError('email já cadastrado', HttpStatus.BAD_REQUEST);
+    }
+
+    const newDonnor = await prisma.donnor.create({ data: donnor });
+
+    return {
+      id: newDonnor.id,
+      name: newDonnor.name,
+      email: newDonnor.email,
+      createdAt: newDonnor.createdAt,
+    };
   }
 }
