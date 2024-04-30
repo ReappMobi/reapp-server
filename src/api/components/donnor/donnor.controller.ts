a39f74d5c4c1d16d55eef7746475c423ff66fb47
Andre Gabriel <andre.gabriel@discente.ufma.br>
2024-04-29 21:45:37 -0300

    feat(api): add error handling to DonnorController createDonnor method
    - Add error handling to the createDonnor method in DonnorController to
    handle exceptions gracefully.

--- src/api/components/donnor/donnor.controller.ts
+++ src/api/components/donnor/donnor.controller.ts
@@ -1,14 +1,18 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 import httpStatus from '@services/http-status';
 import { DonnorService } from './donnor.service';
 
 export const donnor = {
-  async createDonnor(request: Request, response: Response) {
+  async createDonnor(request: Request, response: Response, next: NextFunction) {
     const donnorService = new DonnorService();
     const donnor = request.body;
-    const result = await donnorService.createDonnor(donnor);
-    response.status(httpStatus.CREATED);
-    response.send(result);
+    try {
+      const result = await donnorService.createDonnor(donnor);
+      response.status(httpStatus.CREATED);
+      response.send(result);
+    } catch (error) {
+      next(error);
+    }
   },
 };
