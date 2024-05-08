1a8044620f1a67af3e33a53b1a1878445b8d991c
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-05-07 21:04:21 -0300

    feat: add institution routes in routes.ts

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import healthcheck from '@components/healthcheck/healthcheck.router';
 import donnor from '@components/donnor/donnor.router';
+import institution from '@components/institution/institution.router';
 
 const router: Router = Router();
 
 router.use(healthcheck);
 router.use(donnor);
+router.use(institution);
 
 export default router;
