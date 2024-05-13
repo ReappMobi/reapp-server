a09e01a5407abd86443b239993fd436903ed399b
Andre Gabriel <andre.gabriel@discente.ufma.br>
2024-05-13 11:12:59 -0300

    feat: update donnor.service.ts`
    - Move the logic from classes to function approach.
    - Reduce functionality complexity.

--- src/api/components/donnor/donnor.service.ts
+++ src/api/components/donnor/donnor.service.ts
@@ -1,29 +1,36 @@
 import { Prisma } from '@prisma/client';
-
 import HttpStatus from '@services/http-status';
 import prisma from '@services/client';
-
+import bcrypt from 'bcrypt';
 import { AppError } from 'utils/AppError';
 
-export class DonnorService {
-  public async createDonnor(donnor: Prisma.DonnorCreateInput) {
-    const donnorExists = await prisma.donnor.findUnique({
-      where: {
-        email: donnor.email,
-      },
-    });
+export const createDonnor = async (donnor: Prisma.DonnorCreateInput) => {
+  const { password, email } = donnor;
 
-    if (donnorExists) {
-      throw new AppError('email já cadastrado', HttpStatus.BAD_REQUEST);
-    }
+  const donnorExists = await prisma.donnor.findUnique({
+    where: {
+      email: email,
+    },
+  });
 
-    const newDonnor = await prisma.donnor.create({ data: donnor });
-
-    return {
-      id: newDonnor.id,
-      name: newDonnor.name,
-      email: newDonnor.email,
-      createdAt: newDonnor.createdAt,
-    };
+  if (donnorExists) {
+    throw new AppError('email já cadastrado', HttpStatus.BAD_REQUEST);
   }
-}
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newDonnor = await prisma.donnor.create({
+    data: {
+      ...donnor,
+      password: hashedPassword,
+    },
+  });
+
+  return {
+    id: newDonnor.id,
+    name: newDonnor.name,
+    email: newDonnor.email,
+    createdAt: newDonnor.createdAt,
+  };
+};
