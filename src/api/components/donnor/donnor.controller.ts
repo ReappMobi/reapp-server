2a6313cb9dbc017f931b4860e18249c6a6f36f58
Andre Gabriel <andre.gabriel@discente.ufma.br>
2024-05-13 11:13:47 -0300

    feat: update `donnor.controller.ts`
    - Use the new functional approach in the service.
    - Add error handling.

--- src/api/components/donnor/donnor.controller.ts
+++ src/api/components/donnor/donnor.controller.ts
@@ -1,14 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 
 import httpStatus from '@services/http-status';
-import { DonnorService } from './donnor.service';
+import { createDonnor } from './donnor.service';
+
+interface Donnor {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+}
 
 export const donnor = {
   async createDonnor(request: Request, response: Response, next: NextFunction) {
-    const donnorService = new DonnorService();
-    const donnor = request.body;
+    const donnor = request.body as Donnor;
     try {
-      const result = await donnorService.createDonnor(donnor);
+      const result = await createDonnor(donnor);
       response.status(httpStatus.CREATED);
       response.send(result);
     } catch (error) {
