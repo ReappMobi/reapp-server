16db0bfdd4c041405dea25aae61367f3845831a9
Andre Gabriel <andre.gabriel@discente.ufma.br>
2024-05-13 21:40:53 -0300

    feat: add donor type definitions file

---
+++ src/api/components/donnor/donor.type.ts
@@ -0,0 +1,12 @@
+export type Donor = {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  email: string;
+  password: string;
+  name: string;
+  avatar: string;
+  url: string;
+  uri: string;
+  following_count: number;
+};
