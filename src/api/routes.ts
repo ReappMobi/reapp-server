fb9fe876c25e63f5c33121dbd870e4a4d65d49db
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-05-27 11:38:25 -0300

    feat: add payment routes in routes.ts

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -4,6 +4,7 @@ import healthcheck from '@components/healthcheck/healthcheck.router';
 import donnor from '@components/donnor/donnor.router';
 import institution from '@components/institution/institution.router';
 import auth from '@components/auth/auth.router';
+import payment from '@components/payment/payment.router';
 
 const router: Router = Router();
 
@@ -11,5 +12,6 @@ router.use(healthcheck);
 router.use(donnor);
 router.use(institution);
 router.use(auth);
+router.use(payment);
 
 export default router;
