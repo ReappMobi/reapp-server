3d08f5358a7af9840429b3d306d6eb5a4c1caff4
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-05-27 11:47:03 -0300

    feat: add payment controller

---
+++ src/api/components/payment/payment.controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+import HttpStatus from '@services/http-status';
+import { AppError } from 'utils/AppError';
+import { PaymentService } from './payment.service';
+
+export const payment = {
+  async requestPaymentInstitution(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ) {
+    try {
+      //TODO: middlware for request payment
+      const errors = validationResult(request);
+      if (!errors.isEmpty()) {
+        throw new AppError(errors.array()[0].msg, HttpStatus.BAD_REQUEST);
+      }
+
+      const paymentService = new PaymentService();
+
+      const result = await paymentService.requestPaymentInstitutionUrl(
+        request.body,
+      );
+
+      response.status(HttpStatus.ACCEPTED);
+      response.send({ result });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
