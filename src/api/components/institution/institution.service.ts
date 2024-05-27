09f1134061db47015bb1b23dc079d0f686bebb7e
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-05-27 11:46:40 -0300

    refactor: remove institution sign in service in institution.service

--- src/api/components/institution/institution.service.ts
+++ src/api/components/institution/institution.service.ts
@@ -6,14 +6,6 @@ export class InstitutionService {
   public async institutionSignUp(institution: Prisma.InstitutionCreateInput) {
     const newInstitution = await prisma.institution.create({
       data: institution,
-      include: { profile: true },
-    });
-
-    const newInstitutionProfile = await prisma.institutionProfile.create({
-      data: {
-        displayName: institution.name,
-        institution: { connect: { id: newInstitution.id } },
-      },
     });
 
     return {
@@ -23,24 +15,7 @@ export class InstitutionService {
       phone: newInstitution.phone,
       cnpj: newInstitution.cnpj,
       createdAt: newInstitution.createdAt,
-      profile: newInstitutionProfile,
-    };
-  }
-
-  public async institutionSignIn(email: string) {
-    const institution = await prisma.institution.findUnique({
-      where: { email: email },
-      include: { profile: true },
-    });
-
-    return {
-      id: institution?.id,
-      name: institution?.name,
-      email: institution?.email,
-      phone: institution?.phone,
-      cnpj: institution?.cnpj,
-      createdAt: institution?.createdAt,
-      profile: institution?.profile,
+      avatar: newInstitution.avatar,
     };
   }
 }
