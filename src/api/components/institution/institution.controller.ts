3c746d96c263536be2187fc976cb003bacbab420
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-05-27 11:45:04 -0300

    refactor: remove institution sign in in institution.controller

--- src/api/components/institution/institution.controller.ts
+++ src/api/components/institution/institution.controller.ts
@@ -5,7 +5,6 @@ import { validationResult } from 'express-validator';
 import { AppError } from 'utils/AppError';
 import bcrypt from 'bcrypt';
 import { InstitutionService } from './institution.service';
-import jwt from 'jsonwebtoken';
 
 export const institution = {
   async institutionSignUp(
@@ -20,7 +19,7 @@ export const institution = {
       }
       const institutionService = new InstitutionService();
 
-      const { password, email, name, phone, cnpj } = request.body;
+      const { password, email, name, phone, cnpj, displayName } = request.body;
       const hash = await bcrypt.hash(password, 12);
 
       const result = await institutionService.institutionSignUp({
@@ -28,48 +27,12 @@ export const institution = {
         phone,
         cnpj,
         email,
+        displayName,
         password: hash,
       });
 
       response.status(HttpStatus.CREATED);
       response.send(result);
-    } catch (error) {
-      next(error);
-    }
-  },
-
-  async institutionSignIn(
-    request: Request,
-    response: Response,
-    next: NextFunction,
-  ) {
-    try {
-      const errors = validationResult(request);
-      if (!errors.isEmpty()) {
-        throw new AppError(errors.array()[0].msg, HttpStatus.BAD_REQUEST);
-      }
-
-      const institutionService = new InstitutionService();
-
-      const user = await institutionService.institutionSignIn(
-        request.body.email,
-      );
-
-      const secretKey = process.env.JWT_SECRET;
-
-      if (!secretKey) {
-        throw new AppError(
-          'Chave secreta JWT não configurada',
-          HttpStatus.INTERNAL_SERVER_ERROR,
-        );
-      }
-
-      const token = jwt.sign({ userId: user?.id }, secretKey, {
-        expiresIn: '7d',
-      });
-
-      response.status(HttpStatus.ACCEPTED);
-      response.send({ user: user, token });
     } catch (error) {
       next(error);
     }
