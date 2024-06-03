734887cbf82f900af839085d6660bc4c471c51dd
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-06-02 23:37:28 -0300

    feat: change payment request route and add payment callback route

--- src/api/components/payment/payment.router.ts
+++ src/api/components/payment/payment.router.ts
@@ -3,6 +3,7 @@ import { payment } from './payment.controller';
 
 const router: Router = Router();
 
-router.post('/payment/request/institution', payment.requestPaymentInstitution);
+router.post('/payment/request', payment.requestPaymentInstitution);
+router.post('/payment/callback', payment.paymentCallback);
 
 export default router;
