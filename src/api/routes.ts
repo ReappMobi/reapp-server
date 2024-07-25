6b8ca01614498e9da01bf751b574280b9802f5fb
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-07-24 23:05:39 -0300

    feat: add post routes to router file

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -5,6 +5,7 @@ import donnor from '@components/donnor/donnor.router';
 import institution from '@components/institution/institution.router';
 import auth from '@components/auth/auth.router';
 import payment from '@components/payment/payment.router';
+import post from '@components/post/post.router';
 
 const router: Router = Router();
 
@@ -13,5 +14,6 @@ router.use(donnor);
 router.use(institution);
 router.use(auth);
 router.use(payment);
+router.use(post);
 
 export default router;
