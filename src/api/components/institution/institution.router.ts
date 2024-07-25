1b16c24479e721da7ab2aeb7701be6b9b1a34490
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-07-24 23:13:20 -0300

    feat: update institution.router

--- src/api/components/institution/institution.router.ts
+++ src/api/components/institution/institution.router.ts
@@ -1,14 +1,32 @@
 import { Router } from 'express';
-
+import multer from 'multer';
 import { institution } from './institution.controller';
-import { signUpValidation } from '@api/middlewares/institutionAuthValidation';
+import {
+  signUpValidation,
+  editInformationValidation,
+} from '@api/middlewares/institutionAuthValidation';
+import verifyToken from '@api/middlewares/verifyToken';
 
 const router: Router = Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.post(
   '/institution/signup',
   signUpValidation(),
   institution.institutionSignUp,
 );
+
+router.patch(
+  '/institution/editInformation',
+  verifyToken,
+  editInformationValidation(),
+  upload.single('image'),
+  institution.editInformation,
+);
+
+router.get('/institution', verifyToken, institution.getAllInstitution);
+
+router.get('/institution/:id', verifyToken, institution.getInstitutionByid);
 
 export default router;
